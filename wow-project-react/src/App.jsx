d54676a22fe42e6ffccb4f2a694d5ec8acbda780
Daniel Rud Callesen <Danielrudcallesen96@gmail.com>
2024-04-09 10:38:51 +0200

    Added Footer & Header

--- wow-project-react/src/App.jsx
+++ wow-project-react/src/App.jsx
@@ -1,6 +1,17 @@
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
 
 function App() {
 
+    return (
+        /* <> Is a JSX Fragment needed to have multiple JSX tags */
+        <>
+            <Header></Header>
+            <Footer></Footer>
+        { /* </> Closing Tag of JSX Fragment */ }
+        </>
+
+    )
 }
 
 export default App
